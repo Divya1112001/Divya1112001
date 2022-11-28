e97d6da0a360f1ed33b88d99473a736c64145d11
Divya1112001 <84901890+Divya1112001@users.noreply.github.com>
2022-11-28 08:57:50 +0530

    Create LoginController.cs

---
+++ project/day2/LoginController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginAddManager.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult LoginIndex()
+        {
+            return View();
+        }
+
+    }
+}
