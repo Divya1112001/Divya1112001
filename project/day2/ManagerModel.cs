1a4f4ea49e7a23aa2addada3b350895b90f25288
Divya1112001 <84901890+Divya1112001@users.noreply.github.com>
2022-11-28 08:53:50 +0530

    Create ManagerModel.cs

---
+++ project/day2/ManagerModel.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoginAddManager.Models
+{
+    public class ManagerModel
+    {
+        [Key]
+        public int empid { get; set; }
+     
+        public string empname { get; set; }
+        
+        public string email { get; set; }
+       
+        public DateTime DOB { get; set; }
+        public string gender { get; set; }
+        public string password { get; set; }
+      
+        public string projectid { get; set; }
+        public string deptid { get; set; }
+        public string empdesignation { get; set; }
+      
+        public int phone { get; set; }
+      
+        public DateTime DOJ { get; set; }
+        public string reportmanager { get; set; }
+      
+        public int salary { get; set; }
+        public string status { get; set; }
+    }
+}
